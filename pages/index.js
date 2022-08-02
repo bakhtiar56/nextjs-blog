728703a0647420a99ede632b249a0c38ffe8ccbd
bakhtiar56 <81223935+bakhtiar56@users.noreply.github.com>
2022-08-02 11:36:52 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,52 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/Layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>A motivated computer science student, aspiring to become a full stack software engineer and most importantly a better human being. 
-          Enthusiastic about learning about new technologies in IT world and trying them hands-on.
-
-          Hobbies include reading History books(e.g. Zulfi Bhutto by Wolpert, The First Muslim by Hazelton) and 
-          playing cricket and doing workouts. 
+        <p>
+          Hello, I’m <strong>Shu</strong>. I’m a software engineer and a
+          translator (English/Japanese). You can contact me on{' '}
+          <a href="https://twitter.com/chibicode">Twitter</a>.
         </p>
-      
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
     </Layout>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
