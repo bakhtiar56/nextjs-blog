3f6575e5f336101b8de39d2a89473b6024a14074
Bakhtiar Rasheed <81223935+bakhtiar56@users.noreply.github.com>
2022-11-01 10:23:03 +0500

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,8 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta name="description" content={post.title} />
+
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
